46fc4ca9f94a3a4766f305dfcaf17a66a9f1df2d
James Barnard <james.r.barnard@gmail.com>
2017-02-07 08:21:31 +0000

    work on weekly interval findNext method

--- src/Intervals/WeeklyInterval.php
+++ src/Intervals/WeeklyInterval.php
@@ -2,6 +2,7 @@
 namespace JRBarnard\DateIntervalIterator\Intervals;
 
 use DateTime;
+use DateInterval;
 use JRBarnard\DateIntervalIterator\DateHelper;
 use JRBarnard\DateIntervalIterator\Exceptions\InvalidArgumentException;
 
@@ -20,6 +21,12 @@ class WeeklyInterval implements IntervalInterface
      * @var int
      */
     protected $weeks = 1;
+
+    /**
+     * Storage for usage during the find next loop
+     * @var
+     */
+    protected $lastSetDayOfWeek;
 
     /**
      * @param int $weeks
@@ -94,7 +101,59 @@ class WeeklyInterval implements IntervalInterface
      */
     public function findNextOccurrence(DateTime $current, $direction = self::FORWARDS)
     {
-        // TODO:
-        return $current;
+        return $this->findNext($current, $direction);
+    }
+
+    /**
+     * @param DateTime $current
+     * @param $direction
+     * @param int $daysToAdd
+     *
+     * @return DateTime
+     */
+    protected function findNext(DateTime $current, $direction, $daysToAdd = 0)
+    {
+        $days = $this->getDays();
+        $currentDayOfWeek = DateHelper::getDayOfTheWeek($current);
+
+        if ($direction === self::BACKWARDS) {
+            $days = array_reverse($days);
+        }
+
+        $occurrence = null;
+
+        $count = 0;
+        foreach ($days as $day) {
+            $day += $daysToAdd;
+
+            // Is the current day of the week less than or same as the iterated day?
+            // And has it already been set?
+            if ($currentDayOfWeek <= $day && $day !== $this->lastSetDayOfWeek) {
+
+                // Get next date time by adding the difference in currentDay of week to next day of the week
+                // TODO: extract into helper?
+                // TODO: Get working in reverse
+                $occurrence = (clone $current)->add(new DateInterval('P' . $day - $currentDayOfWeek . 'D'));
+                $occurrenceDayOfWeek = DateHelper::getDayOfTheWeek($occurrence);
+
+                // If it's the not on the same day or it is and it's in the future from the current occurrence,
+                // then we've got our next occurrence and can break the loop.
+                if ($occurrenceDayOfWeek !== $day) {
+                    // set the day of week flag
+                    $this->lastSetDayOfWeek = $occurrenceDayOfWeek;
+                    break;
+                }
+            }
+            $count++;
+
+            // If we've reached the end of the array, add / remove N week(s) and reset the days array as we don't yet have
+            // an occurrence and will look to next / previous week.
+            if ($count >= $daysToAdd) {
+                // TODO: Get working in reverse
+                $occurrence = $this->findNext($current, $direction, $daysToAdd + (7 * $this->getWeeks()));
+            }
+        }
+
+        return $occurrence;
     }
 }
